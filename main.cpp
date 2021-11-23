ebbf6a3a73ba358500b11d6e2f60616222ea80d8
unknown <truongsteven107@gmail.com>
2021-11-23 01:15:08 -0800

    k-fold cross validation

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ float leaveOneOutCrossValidation(const vector<vector<float>>& data, set<int> cur
 
 
 int main() {
-    vector<vector<float>> data = parseData("CS170_Fall_2021_SMALL_data__1.txt");
+    vector<vector<float>> data = parseData("CS170_Fall_2021_SMALL_data__86.txt");
 
     featureSearch(data);
 }
@@ -23,10 +23,13 @@ int main() {
 void featureSearch(const vector<vector<float>>& data) {
 
     set<int> currentSetOfFeatures;
+    vector<int> answer;
+    float bestAccuracy = 0;
+    set<int> bestSet;
 
     for (int i = 1; i < data.at(0).size(); ++i) {
 
-        // cout << "On the " << i << "th level of the search tree" << endl; 
+        cout << "On the " << i << "th level of the search tree" << endl; 
 
         int featureToAddAtThisLevel = -1;
         float bestAccuracySoFar = 0;
@@ -34,7 +37,7 @@ void featureSearch(const vector<vector<float>>& data) {
         for (int k = 1; k < data.at(0).size(); ++k) {
 
             if (currentSetOfFeatures.count(k) > 0) continue;
-            // cout << "--Considering adding the " << k << " feature" << endl;
+            cout << "--Considering adding the " << k << " feature" << endl;
 
             float accuracy = leaveOneOutCrossValidation(data, currentSetOfFeatures, k);
             cout << accuracy << endl;
@@ -42,20 +45,31 @@ void featureSearch(const vector<vector<float>>& data) {
             if (accuracy > bestAccuracySoFar) {
                 bestAccuracySoFar = accuracy;
                 featureToAddAtThisLevel = k;
+
             }
 
         }
 
         currentSetOfFeatures.insert(featureToAddAtThisLevel);
-        for (set<int>::iterator it = currentSetOfFeatures.begin(); it != currentSetOfFeatures.end(); it++) {
-            cout << *it << " ";
+        if (bestAccuracySoFar > bestAccuracy) {
+            bestAccuracy = bestAccuracySoFar;
+            bestSet = currentSetOfFeatures;
+             cout << "BEST SET SO FAR: ";
+            for (auto it = bestSet.begin(); it != bestSet.end(); it++) {
+                cout << *it << " ";
+            }
+            cout << endl;
         }
-        cout << endl;
+    
         // cout << "On level " << i << " added feature " << featureToAddAtThisLevel << " to current set" << endl;
 
     }
 
-
+    for (auto it = bestSet.begin(); it != bestSet.end(); it++) {
+        cout << *it << " ";
+    }
+    cout << "accuracy: " << bestAccuracy << endl;
+    cout << endl;
 }
 
 float leaveOneOutCrossValidation(const vector<vector<float>>& data, set<int> currentSet, int featureToAdd) {
@@ -63,33 +77,42 @@ float leaveOneOutCrossValidation(const vector<vector<float>>& data, set<int> cur
     int numberCorrectlyClassified = 0;
 
     for (int i = 0; i < data.size(); ++i) {
+
         int objectToClassify = static_cast<int>(data.at(i).at(0));
         float nearestNeighborDistance = numeric_limits<float>::max();
         float nearestNeighborLocation = numeric_limits<float>::max();
         int nearestNeighborLabel = -1;
 
+        // cout << "Looping over i, at the " << i << " location" << endl;
+        // cout << "The " << i << "th object is in class " << classNumber << endl;
+        for (int k = 0; k < data.size(); ++k) {
 
-        if (featureToAdd != i) {
-            // cout << "Ask if " << i << " is the nearest neighbor with " << k << endl;
+            if (k != i) {
+                // cout << "Ask if " << i << " is the nearest neighbor with " << k << endl;
 
-            float triangle_sides = 0; //triangle_sides represents //a^2 + b^2 + c^2 + d^2 + ...
-            float distance;
-            
-            for (int j = 0; j < data.at(0).size(); ++j) {
-                    vector<float> row_i = data.at(i); 
-                    vector<float> row_k = data.at(k);
-                triangle_sides += pow(row_i.at(j) - row_k.at(j), 2); 
-            }
+                float triangle_sides = 0; //triangle_sides represents //a^2 + b^2 + c^2 + d^2 + ...
+                float distance;
+                
+                for (int j = 1; j < data.at(0).size(); ++j) {
 
-            distance = sqrt(triangle_sides);  // sqrt(a^2 + b^2 + c^2 + d^2 + ...)
+                    if (currentSet.count(j) > 0 || j == featureToAdd) {
+                        vector<float> row_i = data.at(i); 
+                        vector<float> row_k = data.at(k);
+                        triangle_sides += pow(row_i.at(j) - row_k.at(j), 2); 
+                    }
 
-            if (distance < nearestNeighborDistance) {
-                nearestNeighborDistance = distance;
-                nearestNeighborLocation = k;
-                nearestNeighborLabel = static_cast<int>(data.at(k).at(0));
+
+                }
+
+                distance = sqrt(triangle_sides);  // sqrt(a^2 + b^2 + c^2 + d^2 + ...)
+
+                if (distance < nearestNeighborDistance) {
+                    nearestNeighborDistance = distance;
+                    nearestNeighborLocation = k;
+                    nearestNeighborLabel = static_cast<int>(data.at(k).at(0));
+                }
             }
         }
-
 
         if (objectToClassify == nearestNeighborLabel) numberCorrectlyClassified += 1;
     }
@@ -127,46 +150,5 @@ vector<vector<float>> parseData(const string& filename) {
 }
 
 /*
-float leaveOneOutCrossValidation(const vector<vector<float>>& data, set<int> currentSet, int featureToAdd) {
 
-    int numberCorrectlyClassified = 0;
-
-    for (int i = 0; i < data.size(); ++i) {
-        int objectToClassify = static_cast<int>(data.at(i).at(0));
-        float nearestNeighborDistance = numeric_limits<float>::max();
-        float nearestNeighborLocation = numeric_limits<float>::max();
-        int nearestNeighborLabel = -1;
-
-        // cout << "Looping over i, at the " << i << " location" << endl;
-        // cout << "The " << i << "th object is in class " << classNumber << endl;
-        for (int k = 0; k < data.size(); ++k) {
-
-            if (k != i) {
-                // cout << "Ask if " << i << " is the nearest neighbor with " << k << endl;
-
-                float triangle_sides = 0; //triangle_sides represents //a^2 + b^2 + c^2 + d^2 + ...
-                float distance;
-                
-                for (int j = 0; j < data.at(0).size(); ++j) {
-                     vector<float> row_i = data.at(i); 
-                     vector<float> row_k = data.at(k);
-                    triangle_sides += pow(row_i.at(j) - row_k.at(j), 2); 
-                }
-
-                distance = sqrt(triangle_sides);  // sqrt(a^2 + b^2 + c^2 + d^2 + ...)
-
-                if (distance < nearestNeighborDistance) {
-                    nearestNeighborDistance = distance;
-                    nearestNeighborLocation = k;
-                    nearestNeighborLabel = static_cast<int>(data.at(k).at(0));
-                }
-            }
-        }
-
-        if (objectToClassify == nearestNeighborLabel) numberCorrectlyClassified += 1;
-    }
-
-    cout << numberCorrectlyClassified << ", " << static_cast<float>(data.size()) << endl;
-    return numberCorrectlyClassified / static_cast<float>(data.size());
-}
 */

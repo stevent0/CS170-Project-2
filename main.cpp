174d1b85c9ceb6b64190f09206b28886d76f021d
unknown <truongsteven107@gmail.com>
2021-11-22 22:18:24 -0800

    feature search

--- main.cpp
+++ main.cpp
@@ -3,22 +3,54 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <set>  
 
 using namespace std;
 
-
 vector<vector<float>> parseData(const string& filename);
+void featureSearch(vector<vector<float>>& data);
+float leaveOneOutCrossValidation(const vector<vector<float>>& data, set<float> currentSet, int featureToAdd);
 
 
 int main() {
     vector<vector<float>> data = parseData("CS170_Fall_2021_SMALL_data__1.txt");
 
-    for (vector<float>& row : data) {
-        for (float& x : row) {
-            cout << x << " ";
+    featureSearch(data);
+}
+
+void featureSearch(vector<vector<float>>& data) {
+
+    set<float> currentSetOfFeatures;
+
+    for (int i = 1; i < data.at(0).size(); ++i) {
+
+        cout << "On the " << i << "th level of the search tree" << endl; 
+
+        int featureToAddAtThisLevel;
+        float bestAccuracySoFar = 0;
+
+        for (int k = 1; k < data.at(0).size(); ++k) {
+
+            if (currentSetOfFeatures.count(k) > 0) continue;
+            cout << "--Considering adding the " << k << " feature" << endl;
+
+            float accuracy = leaveOneOutCrossValidation(data, currentSetOfFeatures, k+1);
+
+            if (accuracy > bestAccuracySoFar) {
+                bestAccuracySoFar = accuracy;
+                featureToAddAtThisLevel = k;
+            }
+
         }
-        cout << endl;
+
+        currentSetOfFeatures.insert(featureToAddAtThisLevel);
+        cout << "On level " << i << " added feature " << featureToAddAtThisLevel << " to current set" << endl;
+
     }
+}
+
+float leaveOneOutCrossValidation(const vector<vector<float>>& data, set<float> currentSet, int featureToAdd) {
+    return rand() % 101;
 }
 
 vector<vector<float>> parseData(const string& filename) {
